a6de17b0250ba500a35631d86d3889ba7025778c
walter007 <walter_uol@hotmail.com>
2020-10-01 12:29:55 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/ws/course/entities/User.java
+++ src/main/java/com/ws/course/entities/User.java
@@ -1,13 +1,20 @@
 package com.ws.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -18,6 +25,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User(){
 		
@@ -41,6 +52,10 @@ public class User implements Serializable {
 
 	public String getName() {
 		return name;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public void setName(String name) {
@@ -95,6 +110,8 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 	
 
---
+++ src/main/java/com/ws/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.ws.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ws.course.entities.Order;
+import com.ws.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj =  repository.findById(id);
+		return obj.get();
+	}
+}
