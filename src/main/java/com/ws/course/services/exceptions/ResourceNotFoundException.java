8f78d702d3a1ba9d70eeae9f2f92339fd9112beb
walter007 <walter_uol@hotmail.com>
2020-10-07 12:36:01 -0300

    Exception handling - findById

---
+++ src/main/java/com/ws/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ws.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resouce not found. id " + id);
+	}
+}
